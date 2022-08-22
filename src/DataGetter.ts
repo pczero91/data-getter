4c5515fa8c3c3b128d286957877e1dc9eda5eacc
Pedro Castaneda <pczero91@gmail.com>
2022-08-22 17:52:00 -0400

    Added info about callbacks inputs and define oveloads for getData

--- src/DataGetter.ts
+++ src/DataGetter.ts
@@ -10,25 +10,67 @@ export interface DataHead {
     data?: Buffer
 }
 
-export class DataGetter {
-    static getData(req: IncomingMessage, callback: Function = (): void => {}, final: Function = (): void => {}) {
-        const fp = new FormParser(req);
-        let dataHeaders: DataHead[] = [];
-        let data: Buffer;
-        let maxLength: number = 32;
-        req.on('data', (chunk) => {
-            fp.getData(chunk, (bytes: Buffer, file: string, type: string, name: string) => {
-                callback(bytes, file, type, name);
-                if (!isHeadTaken(dataHeaders, name, file)) {
-                    data = (bytes.length < maxLength) ? bytes : bytes.subarray(0, maxLength)
-                    dataHeaders.push({ name, type, file, data });
-                }
-            });
+export interface DataGetterOptions {
+    callback?: DataGetterCallback,
+    final?: DataGetterFinal
+}
+
+export interface DataGetterCallback {
+    (byte?: Buffer, file?: string, type?: string, name?: string): void
+}
+
+export interface DataGetterFinal {
+    (dataHeaders?: DataHead[]): void
+}
+
+function parseData(req: IncomingMessage, opts: DataGetterOptions) {
+    const fp = new FormParser(req);
+    let dataHeaders: DataHead[] = [];
+    let data: Buffer;
+    let maxLength: number = 32;
+    req.on('data', (chunk) => {
+        fp.getData(chunk, (bytes: Buffer, file: string, type: string, name: string) => {
+            if (opts.callback) {
+                opts.callback(bytes, file, type, name);
+            }
+            if (!isHeadTaken(dataHeaders, name, file)) {
+                data = (bytes.length < maxLength) ? bytes : bytes.subarray(0, maxLength)
+                dataHeaders.push({ name, type, file, data });
+            }
         });
-        req.on('end', () => {
-            final(dataHeaders);
-        });
+    });
+    req.on('end', () => {
+        if (opts.final) {
+            opts.final(dataHeaders);
+        }
+    });
+}
+
+function getData(req:IncomingMessage, cb?: DataGetterOptions): void;
+function getData(req:IncomingMessage, cb?: DataGetterCallback, fn?: DataGetterFinal): void;
+function getData(req:IncomingMessage, cb?: DataGetterCallback | DataGetterOptions, fn?: DataGetterFinal): void {
+    
+    let opts: DataGetterOptions = {
+        callback: (): void => {},
+        final: (): void => {}
     }
+    if (typeof cb === 'function') {
+        opts.callback = cb;
+    }
+    if (fn) {
+        opts.final = fn;
+    }
+    if (typeof cb === 'object' ) {
+        opts.callback = (cb.callback) ? cb.callback : opts.callback;
+        opts.final = (cb.final) ? cb.final : opts.final;
+    }
+    
+    parseData(req, opts);
+}
+
+
+export class DataGetter {
+    static getData = getData;
 }
 
 function isHeadTaken(dataHeaders: DataHead[], name: string = '', file: string = ''): boolean {
